e4150d676e0266b2dde1f020239461bb0dce11b6
SungDaeH <121483266+SungDaeH@users.noreply.github.com>
2022-12-28 19:30:18 +0900

    28일 jquery 소스 연습

--- web/JS/script.js
+++ web/JS/script.js
@@ -6,14 +6,16 @@ function grade(){
   var kr1 = parseInt(document.getElementById("kr1").innerHTML);
   var first = document.getElementById("first_g");
   first.innerHTML=kor1+kr1;
-  first.style.color="red";
+  //first.style.color="red";
+  $('#first_g').css("color","red")
   document.getElementById('first_g').innerHTML=kor1+kr1;
 
   var kor2 = parseInt(document.getElementById("kor2").innerHTML);
   var kr2 = parseInt(document.getElementById("kr2").innerHTML);
   var second = document.getElementById("second_g");
   second.innerHTML=kor2+kr2;
-  second.style.color="red";
+  //second.style.color="red";
+  $('#second_g').css("color","green")
   document.getElementById('second_g').innerHTML=kor2+kr2;
 
   var kor3 = parseInt(document.getElementById("kor3").innerHTML);
@@ -21,6 +23,13 @@ function grade(){
   var third = document.getElementById("third_g");
   document.getElementById('third_g').innerHTML=kor3+kr3;
   third.innerHTML=kor3+kr3;
-  third.style.color="red";
+  //third.style.color="red";
+  $('#third_g').css("color","blue")
   alert("성적이 확인되었습니다.")
+  //document.getElementById('total').style.fontSize= '30px'
+  // $('#total').css({
+  //   fontSize: 30
+  // });
+  $('#total').css("fontSize",30);
+  $('#total').css("fontSize","30px")
 }
